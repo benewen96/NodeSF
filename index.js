db9f959ae615b442d4b44f1ad4623407416d4e68
Ben Ewen <Ben Ewen>
2016-08-06 16:16:23 +0100

    consolidated record methods into module

--- index.js
+++ index.js
@@ -1,65 +1,9 @@
 var SFserver = require("./conn.js");
-var createRecord = require("./createRecord.js");
+var recordIO = require("./record.js");
 var express = require("express");
 var conn = undefined;
 
-//create a new record object
-var newRecord = function(err) {
-    SFserver.Connect(function(err, connection) { //connect to sf
-        if(err) {
-           return(err);
-        }
-        else {
-            console.log('Connect success');
-            conn = connection;  //pull connection to index.js
-            createRecord.Create(conn, function(err, ret) { //call create record method
-                if(err) {
-                    return(err);
-                }
-                else {
-                    return('RECORD CREATED: ' + ret.id);
-                }
-            });
-        }
-    });
-}
 
-
-var deleteRecords = function(callback) { //delete records will return to a user defined callback when appropriate
-//callback trace (2) -------|________|
-	var queryRes = [];
-	SFserver.Connect(function(err, connection) { //connect to sf
-	    if(err) {
-	       	return(err);
-	    }
-	    else {
-	    	conn = connection;
-	    	
-	    	var deleteResults = function() {
-				conn.sobject("Record__c")
-					.find({ Name : 'I am a node object' })
-					.destroy(function(err, rets) {
-						if(err) {return console.error(err); }
-						//something useful to return to our callback
-						return callback(queryRes.length + ' records deleted from salesforce'); //trace start (1)
-					});
-			}
-
-			conn.query("select Name from Record__c")
-			.on("record", function(record) { //on emit of a record
-				queryRes.push(record);
-			})
-			.on("end", function(record) {	 //when query finished
-				deleteResults();
-			})
-			.on("error", function(error) {   //on emit of an error
-				console.error(err); 
-			}).run();
-
-			
-		}
-	});
-}
 
 
 
@@ -67,19 +11,23 @@ var deleteRecords = function(callback) { //delete records will return to a user 
 var server = express();
 
 server.get('/', function(req, res) {
-	res.send(newRecord());
+	res.send('Welcome');
 });
+
+server.get('/create', function(req, res) {
+	var info = undefined;
+	recordIO.newRecord(function(info) {
+		res.send(info);
+	});
+});
+
 
 server.get('/del', function(req, res) {
 	var info = undefined;
 	//execute delete records function, when it completes, call function(info) callback
-	deleteRecords(/*my callback def*/function(info) { //callback trace finish (3)
+	recordIO.deleteRecords(/*my callback def*/function(info) { //callback trace finish (3)
 		res.send(info);
 	});
-
-	/* connect to sf server and run event-driven soql query, collate results in queryRes */
-	 
-
 });
 
 server.listen(3000, function () {
---
+++ record.js
@@ -0,0 +1,58 @@
+var SFserver = require("./conn.js");
+
+module.exports = {
+    //params: conn: connection instance, callback: return object
+    newRecord : function(callback) {
+    SFserver.Connect(function(err, connection) { //connect to sf
+    	if(err) {
+    		return(err);
+    	}
+    	else {
+    		console.log('Connect success');
+
+    		connection.sobject("Record__c").create({ Name : 'I am a node object' }, function(err, ret) {
+	            if (err || !ret.success) { return callback(err); } //if internal error or sf error return the error
+	            return callback('RECORD CREATED: ' + ret.id);
+        	});
+
+        	}
+    	});
+	},
+
+	deleteRecords : function(callback) { //delete records will return to a user defined callback when appropriate
+	//callback trace (2) -------|________|
+	var queryRes = [];
+	SFserver.Connect(function(err, connection) { //connect to sf
+	    if(err) {
+	       	return(err);
+	    }
+	    else {
+	    	var deleteResults = function() {
+				connection.sobject("Record__c")
+					.find({ Name : 'I am a node object' })
+					.destroy(function(err, rets) {
+						if(err) {return console.error(err); }
+						//something useful to return to our callback
+						return callback(queryRes.length + ' records deleted from salesforce'); //trace start (1)
+					});
+			}
+
+			connection.query("select Name from Record__c")
+			.on("record", function(record) { //on emit of a record
+				queryRes.push(record);
+			})
+			.on("end", function(record) {	 //when query finished
+				deleteResults();
+			})
+			.on("error", function(error) {   //on emit of an error
+				console.error(err); 
+			}).run();
+
+			
+		}
+	});
+}
+
+}
+
+
